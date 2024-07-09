e4dbed90a6ef30b79f7ef0112b2c685d5663e3da
Quartic <suhel@quartic.ai>
2024-07-09 19:50:22 +0530

    Lazy load pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-import { HomePage } from "./pages/home";
-import { ContextPage } from "./pages/context";
-import { ZustandPage } from "./pages/zustand";
-import { RecoilPage } from "./pages/recoil";
-import { JotaiPage } from "./pages/jotai";
-import { MobXPage } from "./pages/mobx";
-
 import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme";
+import { lazy } from "react";
+
+const HomePage = lazy(() =>
+  import("./pages/home").then((module) => ({ default: module.HomePage })),
+);
+const ContextPage = lazy(() =>
+  import("./pages/context").then((module) => ({ default: module.ContextPage })),
+);
+const ZustandPage = lazy(() =>
+  import("./pages/zustand").then((module) => ({ default: module.ZustandPage })),
+);
+const RecoilPage = lazy(() =>
+  import("./pages/recoil").then((module) => ({ default: module.RecoilPage })),
+);
+const JotaiPage = lazy(() =>
+  import("./pages/jotai").then((module) => ({ default: module.JotaiPage })),
+);
+const MobXPage = lazy(() =>
+  import("./pages/mobx").then((module) => ({ default: module.MobXPage })),
+);
 
 const Layout = () => {
   return (
