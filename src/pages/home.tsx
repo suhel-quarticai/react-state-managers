d0df5dba9621c8d7b1bec4d42e8823600d049bbd
Quartic <suhel@quartic.ai>
2024-07-11 15:32:09 +0530

    Add redirect to context for /

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export const HomePage = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  useEffect(() => navigate("/context"), []);
+
+  return <>x</>;
 };
